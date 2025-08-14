8f8e946a4e54bf20bfce5b1c7fee493d9e1ded3e
edwar <edwardsameh99@gmail.com>
2025-08-14 22:41:55 +0300

    Assignment

---
+++ Assignment/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Book(string _ISBN, string Title, string[] _Authors, DateTime PublicationDate, decimal _Price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment
+{
+    public class Program
+    {
+        // a) Create User Defined Delegate with the same signature of methods existed in Bookfunctions class.
+        public delegate string BookFunctionsDelegate(Book book);
+
+        static void Main(string[] args)
+        {
+            // b) Use the Proper build in delegate. 
+            Func<Book, String> func = BookFunctions.GetTitle;
+
+            // C) Anonymous Method (GetISBN).
+            Func<Book, String> GetISBN = delegate (Book book) { return book.ISBN; };
+
+            // D) Lambda Expression (GetPublicationDate)
+            Func<Book, DateTime> GetPublicationDate = book => book.PublicationDate;
+
+        }
+    }
+}
